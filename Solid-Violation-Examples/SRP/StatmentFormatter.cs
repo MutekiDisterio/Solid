beaa733eb494736747d4462e61998b4743e5ea83
MutekiDisterio <dmelenko@gmail.com>
2025-05-13 17:20:49 +0300

    s-solid

---
+++ Solid-Violation-Examples/SRP/StatmentFormatter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Violation_Examples.SRP
+{
+    public class StatementFormatter
+    {
+        private const string DATE_FORMAT = "dd/MM/yyyy";
+        private const string AMOUNT_FORMAT = "F2";
+
+        public string FormatHeader()
+        {
+            return "DATE | AMOUNT | BALANCE";
+        }
+        public string StatementLine(Transaction transaction, int balance) =>
+            $"{FormatDate(transaction.Date)} | {FormatNumber(transaction.Amount)} | {FormatNumber(balance)}";
+        private string FormatDate(DateOnly date) =>
+            date.ToString(DATE_FORMAT, CultureInfo.InvariantCulture);
+        private string FormatNumber(int amount) =>
+            amount.ToString(AMOUNT_FORMAT, CultureInfo.InvariantCulture);
+    }
+}
---
+++ Solid-Violation-Examples/SRP/StatmentPrinter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Violation_Examples.SRP
+{
+    public class StatementPrinter
+    {
+        private readonly ConsolePrinter _printer;
+        private readonly StatementFormatter _formatter;
+
+        public StatementPrinter(ConsolePrinter printer, StatementFormatter formatter)
+        {
+            _printer = printer;
+            _formatter = formatter;
+        }
+        public void Print(List<Transaction> transactions)
+        {
+            int balance = 0;
+            _printer.PrintLine(_formatter.FormatHeader());
+
+            foreach (var transaction in transactions.OrderByDescending(t => t.Date))
+            {
+                balance += transaction.Amount;
+                _printer.PrintLine(_formatter.StatementLine(transaction, balance));
+            }
+        }
+    }
+}
